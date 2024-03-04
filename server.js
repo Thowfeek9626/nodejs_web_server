7841ffbe255cd80c7fe7ef71e4d14906a9dc5ae3
Mohamed <mohamedthowfeek9626@gmail.com>
2024-03-04 06:47:36 +0400

    routing , mvc rest api

--- server.js
+++ server.js
@@ -5,22 +5,12 @@ const cors = require('cors');
 const {logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
+const corsOptions = require('./config/corsOptions')
 
 // custom middleware logger
 app.use(logger)
 
 // Cross Origin Resource Sharing
-const whiteList = ['https://www.google.com','http://127.0.0.1:5500','http://localhost:3500'];
-const corsOptions = {
-    origin:(origin,callback)=>{
-        if(whiteList.indexOf(origin) !== -1 || !origin){
-            callback(null,true)
-        }else{
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus:200
-}
 app.use(cors(corsOptions))
 
 // built-in middleware to handle url encoded data
@@ -32,49 +22,11 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 //built-in middleware to serve static files
-app.use(express.static(path.join(__dirname,'/public')));
+app.use('/',express.static(path.join(__dirname,'/public')));
+app.use('/subdir',express.static(path.join(__dirname,'/public')));
 
-
-app.get('^/$|/index(.html)?',(req,res)=>{
-    // res.sendFile('./views/index.html',{root:__dirname});
-    res.sendFile(path.join(__dirname,'views','index.html'));
-});
-
-app.get('/new-page(.html)?',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views','new-page.html'));
-});
-
-app.get('/old-page(.html)?',(req,res)=>{
-    res.redirect(301,'new-page.html');
-});
-
-//route handlers
-app.get('/hello(.html)?',(req,res,next)=>{
-    console.log('attempted to load hello.html');
-    next()
-},(req,res)=>{
-    res.sendFile('Hello World!')
-});
-
-
-// chaining route handlers
-
-const one = (req,res,next)=>{
-    console.log('one');
-    next();
-}
-
-const two = (req,res,next)=>{
-    console.log('two');
-    next();
-}
-
-const three = (req,res)=>{
-    console.log('three');
-    res.send('Finished');
-}
-
-app.get('/chain(.html)?',[one ,two ,three ]);
+app.use('/',require('./routes/root'))
+app.use('/employees',require('./routes/api/employees'))
 
 app.all('*',(req,res)=>{
     res.status(404);
